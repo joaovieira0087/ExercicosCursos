b744a5978aeabc3cbe152af0f0ec14a7b44b657e
joaovieira0087 <joaovieira0087@gmail.com>
2026-07-21 00:11:43 -0300

    par impar

--- Program.cs
+++ Program.cs
@@ -131,4 +131,16 @@ else
     }
 */
 
+Console.Write("digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("par");
+}
+else
+{
+    Console.WriteLine("impar");
+}
+
 
