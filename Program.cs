631b53b1d61755c86204c1b33ba141fc8dcf05b7
joaovieira0087 <joaovieira0087@gmail.com>
2026-08-13 15:34:18 -0300

    numeros divisores for

--- Program.cs
+++ Program.cs
@@ -507,3 +507,16 @@ for (int i = 0; i < n; i++)
     }
 }
 */
+
+
+int x = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= x; i++)
+{
+    if(x % i  == 0)
+    {
+        Console.WriteLine("numeros que são divisores")
+        Console.WriteLine(i);
+    }
+}
+Console.ReadKey();
